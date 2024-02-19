58d21df413528799a4cb2c5be204af82407afe51
Khishamuddin Syed <kmsyed2003@gmail.com>
2024-02-19 22:28:58 +0530

    create loader effect JavaScript file

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener to window's load event
+window.addEventListener('load', function () {
+  // Add loaded class to body to hide loader and show content
+  document.body.classList.add('loaded');
+});
